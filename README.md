f5f42c3aa09cbe5b31499221afba3f4fc6843202
rioangele2 <a1921292838@gmail.con>
2023-12-23 16:58:10 +0800

    update: readme

--- README.md
+++ README.md
@@ -1,4 +1,19 @@
-It removes many custom methods and plug-in configurations that are not useful to me, 
-leaving only the more original configurations. 
+It removes many custom methods and plug-in configurations that are not useful to me,
+
+leaving only the more original configurations.
+
 This makes it easier to configure it to your liking.
 
+# 2023-12-23
+
+I make a lot of changes to this project
+
+I think that it deviated from the original intention of doing this project.
+
+I just want to reproduce the configuration of `Lazyvim` , and learn how to configure the `neovim`.
+
+but I always want to make some things for myself which escape the `lazyvim`.
+
+so I decided to leave this project and make a new project for myself .
+
+so this one is not updated.
--- lua/plugins/coding/surround.lua
+++ lua/plugins/coding/surround.lua
@@ -19,14 +19,14 @@ local M = {
 				change_line = "gsC",
 			},
 		})
-		local map = vim.keymap.set
-		map({ "n", "v" }, "gsa", "gsa", { desc = "surround insert", silent = true, noremap = true })
-		map({ "n", "v" }, "gsl", "gsl", { desc = "surround insert line", silent = true, noremap = true })
-		map({ "n" }, "gsA", "gsA", { desc = "surround insert current", silent = true, noremap = true })
-		map({ "n" }, "gsL", "gsL", { desc = "surround insert current line", silent = true, noremap = true })
-		map({ "n" }, "gsd", "gsd", { desc = "surround delete", silent = true, noremap = true })
-		map({ "n" }, "gsc", "gsc", { desc = "surround change", silent = true, noremap = true })
-		map({ "n" }, "gsC", "gsC", { desc = "surround change line", silent = true, noremap = true })
+		-- local map = vim.keymap.set
+		-- map({ "n", "v" }, "gsa", "gsa", { desc = "surround insert", silent = true, noremap = true })
+		-- map({ "n", "v" }, "gsl", "gsl", { desc = "surround insert line", silent = true, noremap = true })
+		-- map({ "n" }, "gsA", "gsA", { desc = "surround insert current", silent = true, noremap = true })
+		-- map({ "n" }, "gsL", "gsL", { desc = "surround insert current line", silent = true, noremap = true })
+		-- map({ "n" }, "gsd", "gsd", { desc = "surround delete", silent = true, noremap = true })
+		-- map({ "n" }, "gsc", "gsc", { desc = "surround change", silent = true, noremap = true })
+		-- map({ "n" }, "gsC", "gsC", { desc = "surround change line", silent = true, noremap = true })
 	end,
 }
 return M
